22003f68bdb2854e32fd63a475e008e7cf644794
Shekhar Joshi <shekharanna1993@gmail.com>
2017-07-12 00:07:39 +0530

    Init commit with vue router

---
+++ src/pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{msg}}</div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data() {
+      return {
+        msg: 'this is router'
+      }
+    }
+  }
+</script>
---
+++ src/router/routes.js
@@ -0,0 +1,7 @@
+export default [
+  {
+    name: 'login',
+    path: '/login',
+    component: require('../pages/login.vue')
+  }
+]
